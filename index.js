13f7f77613005fe6cce57eb2c55a978b3b152ec6
sunshine-agoo <sun.molina@gmail.com>
2024-04-19 21:19:27 -0400

    Listen on port and log server status

--- index.js
+++ index.js
@@ -29,3 +29,8 @@ app.use("/videos", videosRouter);
 app.use((req, res, next) => {
     res.status(404).send("Sorry can't find that!");
 });
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
